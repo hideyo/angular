5fdb77063fd5ed0c7172b5ddf86e085a55e2ec1f
Kenny Korte <kenny.korte@gmail.com>
2017-12-21 23:14:30 +0100

    Start with product pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ProductModule } from './product/product.module';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    ProductModule,
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
